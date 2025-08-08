94430aad52f3adc73ea8793130f22ca4bbf82829
Prabhjot Singh <ps459811@gmail.com>
2025-08-08 14:00:44 +0530

    added new blog button

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -14,6 +14,9 @@ export const Appbar = () => {
   function handleClick(){
   navigate("/Profile")
 }
+  function handleClick2(){
+    navigate("/publish")
+  }
 
   return (
     <div className="border-b flex justify-between items-center px-10 py-5">
@@ -22,19 +25,13 @@ export const Appbar = () => {
         <div className="text-2xl font-semibold cursor-pointer">Medium</div>
       </Link>
 
-
       <div className="flex items-center gap-4">
-        {pathname.startsWith("/blogs") && (
-          <Link to="/publish">
-            <NewBlogButton label="New Blog +" />
-          </Link>
-        )}
+        <button className="bg-amber-950" onClick={handleClick2}>
+          New Blog
+        </button>
         <button className="cursor-pointer " onClick={handleClick} >
         <Avatar name={username} size="big" />
         </button>
-        <div>
-          hello
-        </div>
       </div>
     </div>
   )
