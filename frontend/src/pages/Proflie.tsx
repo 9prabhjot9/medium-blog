01f492bac00d396e819a5fcb10724e258f85c5a3
Prabhjot Singh <ps459811@gmail.com>
2025-08-24 21:52:20 +0530

    added header in profile

--- frontend/src/pages/Proflie.tsx
+++ frontend/src/pages/Proflie.tsx
@@ -1,5 +1,17 @@
+import { Appbar } from "@/components/Appbar"
+import { Link } from "react-router-dom"
+
 export const Profile = () => {
     return <div>
+        <div className="border-b flex justify-between items-center px-10 py-5">
+
+      <Link to="/blogs">
+        <div className="text-2xl font-semibold cursor-pointer">Medium</div>
+      </Link>
+
+      <div className="flex items-center gap-4">
+</div>
+</div>
         Hello from profile
     </div>
 }
